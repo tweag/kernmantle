16953c45cdc58e9818162cc9f2cc89f1d60ae20b
Yves Parès <yves.pares@gmail.com>
2019-11-27 18:31:57 +0100

    Added NamedStrand alias

--- src/Control/Arrow/Twine.hs
+++ src/Control/Arrow/Twine.hs
@@ -14,7 +14,7 @@ module Control.Arrow.Twine
   , Tannen(..)
   , Twine(..)
   , TightTwine, LooseTwine
-  , Strand, NamedStrands, TwineRec
+  , Strand, NamedStrand, TwineRec
   , StrandRunner(..)
   , tightenTwine, loosenTwine
   , type (~>)
@@ -43,10 +43,10 @@ import Prelude hiding (id, (.))
 type Strand = * -> * -> *
 
 -- | The kind for a type-level effect list
-type NamedStrands = [(Symbol, Strand)]
+type NamedStrand = (Symbol, Strand)
 
 -- | The kind for records that will contain 'StrandRunner's
-type TwineRec = ((Symbol, Strand) -> *) -> NamedStrands -> *
+type TwineRec = (NamedStrand -> *) -> [NamedStrand] -> *
 
 type family Snd t where
   Snd '(a,b) = b
@@ -55,16 +55,16 @@ type family Snd t where
 type f ~> g = forall x y. f x y -> g x y
 
 -- | Runs one strand (* -> * -> * effect) in another
-newtype StrandRunner (sup::Strand) (strand::(Symbol,Strand)) = StrandRunner
+newtype StrandRunner (sup::Strand) (strand::NamedStrand) = StrandRunner
   { runStrand :: Snd strand ~> sup }
 
 -- | 'Twine_' is a free arrow built out of _several_ arity 2 effects
 -- (ie. effects with kind * -> * -> *). These effects are called 'Strand's, and
 -- they can be interlaced "on top" of an existing arrow @sup@ (for "support").
 --
--- Note that if @sup@ is maintained polymorphic, then this corresponds to the
--- final encoding of the free arrow construction.
-newtype Twine (record::TwineRec) (strands::NamedStrands) (sup::Strand) a b =
+-- Note that if @sup@ is maintained universally quantified, then this
+-- corresponds to the final encoding of the free arrow construction.
+newtype Twine (record::TwineRec) (strands::[NamedStrand]) (sup::Strand) a b =
   Twine { runTwine :: record (StrandRunner sup) strands -> sup a b }
   deriving (Category, Arrow, ArrowChoice, ArrowLoop, ArrowZero, ArrowPlus
            ,Bifunctor)
