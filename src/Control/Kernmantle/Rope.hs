670dea27b8e5b6cbc51ac6007b3874b344fb4c48
Yves Parès <yves.pares@gmail.com>
2019-12-03 18:32:24 +0100

    RopeRunner moved to Internal.hs

--- src/Control/Kernmantle/Rope.hs
+++ src/Control/Kernmantle/Rope.hs
@@ -9,7 +9,6 @@
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -46,6 +45,7 @@ module Control.Kernmantle.Rope
   , type (:->)
   , (&)
 
+
   , tighten, loosen
   , entwine
   , retwine
@@ -64,15 +64,12 @@ where
 
 import Control.Category
 import Control.Arrow
-import Control.Monad.Trans.Reader
 import Data.Bifunctor
 import Data.Bifunctor.Tannen
 import Data.Bifunctor.Product
-import Data.Profunctor hiding (rmap)
-import Data.Profunctor.Cayley
-import Data.Profunctor.Sieve
 import Data.Function ((&))
-import Data.Functor.Identity
+import Data.Profunctor hiding (rmap)
+import Data.Profunctor.Sieve
 import Data.Typeable
 import Data.Vinyl hiding ((<+>))
 import Data.Vinyl.ARec
@@ -86,58 +83,8 @@ import Prelude hiding (id, (.))
 
 import Control.Kernmantle.Error
 import Control.Kernmantle.Functors
+import Control.Kernmantle.Rope.Internal
 
-
--- | The kind for all binary effects. First param is usually an input
--- (contravariant) of the effect and second one an output (covariant).
-type BinEff = * -> * -> *
-
--- | The kind for unary effects
-type UnaryEff = * -> *
-
--- | The kind for a named binary effect. Must remain a tuple because that's what
--- vinyl expects.
-type Strand = (Symbol, BinEff)
-
-type family StrandName t where
-  StrandName '(name, eff) = name
-
-type family StrandEff t where
-  StrandEff '(name, eff) = eff
-
--- | The kind for records that will contain 'Weaver's. First type param will
--- most often be @Weaver someCore@
-type RopeRec = (Strand -> *) -> [Strand] -> *
-
--- | Runs one @strand@ (* -> * -> * effect) in a @interp@ effect. Is
--- parameterized over a Strand (and not just a BinEffect) even if it ignores its
--- name internally because that's what is expect by the 'RopeRec'
-newtype Weaver (interp::BinEff) (strand::Strand) = Weaver
-  { weaveStrand :: StrandEff strand :-> interp }
-
-mapWeaverInterp :: (interp :-> interp')
-                -> Weaver interp strand
-                -> Weaver interp' strand
-mapWeaverInterp f (Weaver w) = Weaver $ f . w
-{-# INLINE mapWeaverInterp #-}
-
-newtype RopeRunner (record::RopeRec) (mantle::[Strand]) (interp::BinEff) (core::BinEff) a b =
-  RopeRunner (record (Weaver interp) mantle -> core a b)
-  
-  deriving ( Category, Bifunctor
-           , Arrow, ArrowChoice, ArrowLoop, ArrowZero, ArrowPlus
-           , Closed, Costrong, Cochoice
-           , ThrowEffect ex, TryEffect ex
-           )
-    via Reader (record (Weaver interp) mantle) `Tannen` core
-
-  deriving (Profunctor, Strong, Choice)
-    via Reader (record (Weaver interp) mantle) `Cayley` core
-
-instance (RMap m) => EffProfunctor (RopeRunner Rec m) where
-  effdimap f g (RopeRunner run) = RopeRunner $
-    g . run . rmap (mapWeaverInterp f)
-  {-# INLINE effdimap #-}
 
 -- | 'Rope' is a free arrow built out of _several_ binary effects (ie. effects
 -- with kind * -> * -> *). These effects are called 'Strand's, they compose the
@@ -231,12 +178,6 @@ asCore :: core :-> Rope r mantle core
 asCore = mkRope . const
 {-# INLINE asCore #-}
 
--- | Indicates that @strands@ can be reordered and considered a subset of
--- @strands'@
-type RetwinableAs record strands core strands' =
-  ( RecSubset record strands strands' (RImage strands strands')
-  , RecSubsetFCtx record (Weaver core) )
-
 -- | Reorders the strands to match some external context. @strands'@ can contain
 -- more elements than @strands@. Note it works on both 'TightRope's and
 -- 'LooseRope's
@@ -245,25 +186,6 @@ retwine :: (RetwinableAs r strands core strands')
        :-> Rope r strands' core
 retwine r = mkRope $ runRope r . rcast
 {-# INLINE retwine #-}
-
--- | Splits a 'RopeRunner' in two parts, so we can select several strands to act on
-splitRopeRunner :: RopeRunner Rec (mantle1 ++ mantle2) interp core
-               :-> RopeRunner Rec mantle1 interp (RopeRunner Rec mantle2 interp core)
-splitRopeRunner (RopeRunner f) = RopeRunner $ \r1 -> RopeRunner $ \r2 ->
-  f $ r1 `rappend` r2
-
-joinRopeRunner :: (RetwinableAs r mantle1 interp mantle
-                  ,RetwinableAs r mantle2 interp mantle)
-               => RopeRunner r mantle1 interp (RopeRunner r mantle2 interp core)
-              :-> RopeRunner r mantle interp core
-joinRopeRunner (RopeRunner f) = RopeRunner $ \r -> case f (rcast r) of
-  RopeRunner f' -> f' (rcast r)
-
--- -- API-wise, it'd be better if splitRope could be reformulated as the
--- -- following, but that seems harder to implement:
--- groupStrands :: LooseRope (mantle1 ++ mantle2) core a b
---              -> LooseRope ('(name,LooseRope mantle1 core) ': mantle2) core a b
--- groupStrands (Rope f) = Rope $ \(Weaver subrope :& r) ->  
 
 -- | Merge two strands that have the same effect type. Keeps the first name.
 mergeStrands :: Label n1
@@ -302,39 +224,6 @@ toSieve = Kleisli
 toSieve_ :: ueff x -> ToSieve ueff () x
 toSieve_ = Kleisli . const
 {-# INLINE toSieve_ #-}
-
--- | Wrap each strand effect inside a type constructor
-type family MapStrandEffs f mantle where
-  MapStrandEffs f '[] = '[]
-  MapStrandEffs f ( s ': strands ) = '(StrandName s, f (StrandEff s)) ': MapStrandEffs f strands
-
-effmapRopeRec :: (EffFunctor f)
-              => Rec (Weaver interp) strands
-              -> Rec (Weaver (f interp)) (MapStrandEffs f strands)
-effmapRopeRec RNil = RNil
-effmapRopeRec (Weaver w :& rest) = Weaver (effmap w) :& effmapRopeRec rest
-
--- | When all the strands of a 'Rope' have the same type of ahead of time
--- effects, we can run them. See 'splitRope' to isolate some strands in a 'Rope'
-unwrapRopeRunner :: (EffFunctor f)
-                 => RopeRunner Rec (MapStrandEffs f strands) (f core) core
-                :-> RopeRunner Rec strands core core
-unwrapRopeRunner (RopeRunner f) = RopeRunner $ f . effmapRopeRec
-{-# INLINE unwrapRopeRunner #-}
-
-unwrapSomeStrands :: (EffFunctor f, RMap (MapStrandEffs f mantle1))
-                  => (f core' :-> interp)
-                     -- ^ How to run the extracted EffFunctor layer
-                  -> (RopeRunner Rec mantle2 interp core :-> core')
-                     -- ^ What to do with the remaining strands (those not
-                     -- wrapped)
-                  -> RopeRunner Rec (MapStrandEffs f mantle1 ++ mantle2) interp core
-                     -- ^ The 'RopeRunner' to transform, where first strands all
-                     -- are wrapped in the @f@ EffFunctor
-                 :-> RopeRunner Rec mantle1                              core'  core'
-                     -- ^ The resulting 'RopeRunner', where 
-unwrapSomeStrands f g = unwrapRopeRunner . effdimap f g . splitRopeRunner
-{-# INLINE unwrapSomeStrands #-}
 
 entwineEffFunctors :: (EffFunctor f, RMap (MapStrandEffs f mantle1))
                    => (f core' :-> core)  -- ^ Run the effects
---
+++ src/Control/Kernmantle/Rope/Internal.hs
@@ -0,0 +1,133 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Control.Kernmantle.Rope.Internal where
+
+import Control.Category
+import Control.Arrow
+import Control.Monad.Trans.Reader
+import Data.Profunctor hiding (rmap)
+import Data.Bifunctor
+import Data.Bifunctor.Tannen
+import Data.Functor.Identity
+import Data.Profunctor.Cayley
+import Data.Vinyl hiding ((<+>))
+import Data.Vinyl.TypeLevel
+import GHC.TypeLits
+
+import Prelude hiding (id, (.))
+
+import Control.Kernmantle.Error
+import Control.Kernmantle.Functors
+
+
+-- | The kind for all binary effects. First param is usually an input
+-- (contravariant) of the effect and second one an output (covariant).
+type BinEff = * -> * -> *
+
+-- | The kind for unary effects
+type UnaryEff = * -> *
+
+-- | The kind for a named binary effect. Must remain a tuple because that's what
+-- vinyl expects.
+type Strand = (Symbol, BinEff)
+
+type family StrandName t where
+  StrandName '(name, eff) = name
+
+type family StrandEff t where
+  StrandEff '(name, eff) = eff
+
+-- | The kind for records that will contain 'Weaver's. First type param will
+-- most often be @Weaver someCore@
+type RopeRec = (Strand -> *) -> [Strand] -> *
+
+-- | Runs one @strand@ (* -> * -> * effect) in a @interp@ effect. Is
+-- parameterized over a Strand (and not just a BinEffect) even if it ignores its
+-- name internally because that's what is expect by the 'RopeRec'
+newtype Weaver (interp::BinEff) (strand::Strand) = Weaver
+  { weaveStrand :: StrandEff strand :-> interp }
+
+mapWeaverInterp :: (interp :-> interp')
+                -> Weaver interp strand
+                -> Weaver interp' strand
+mapWeaverInterp f (Weaver w) = Weaver $ f . w
+{-# INLINE mapWeaverInterp #-}
+
+newtype RopeRunner (record::RopeRec) (mantle::[Strand]) (interp::BinEff) (core::BinEff) a b =
+  RopeRunner (record (Weaver interp) mantle -> core a b)
+  
+  deriving ( Category, Bifunctor
+           , Arrow, ArrowChoice, ArrowLoop, ArrowZero, ArrowPlus
+           , Closed, Costrong, Cochoice
+           , ThrowEffect ex, TryEffect ex
+           )
+    via Reader (record (Weaver interp) mantle) `Tannen` core
+
+  deriving (Profunctor, Strong, Choice)
+    via Reader (record (Weaver interp) mantle) `Cayley` core
+
+instance (RMap m) => EffProfunctor (RopeRunner Rec m) where
+  effdimap f g (RopeRunner run) = RopeRunner $
+    g . run . rmap (mapWeaverInterp f)
+  {-# INLINE effdimap #-}
+
+-- | Splits a 'RopeRunner' in two parts, so we can select several strands to act on
+splitRopeRunner :: RopeRunner Rec (mantle1 ++ mantle2) interp core
+               :-> RopeRunner Rec mantle1 interp (RopeRunner Rec mantle2 interp core)
+splitRopeRunner (RopeRunner f) = RopeRunner $ \r1 -> RopeRunner $ \r2 ->
+  f $ r1 `rappend` r2
+
+-- | Indicates that @strands@ can be reordered and considered a subset of
+-- @strands'@
+type RetwinableAs record strands core strands' =
+  ( RecSubset record strands strands' (RImage strands strands')
+  , RecSubsetFCtx record (Weaver core) )
+
+joinRopeRunner :: (RetwinableAs r mantle1 interp mantle
+                  ,RetwinableAs r mantle2 interp mantle)
+               => RopeRunner r mantle1 interp (RopeRunner r mantle2 interp core)
+              :-> RopeRunner r mantle interp core
+joinRopeRunner (RopeRunner f) = RopeRunner $ \r -> case f (rcast r) of
+  RopeRunner f' -> f' (rcast r)
+
+-- | Wrap each strand effect inside a type constructor
+type family MapStrandEffs f mantle where
+  MapStrandEffs f '[] = '[]
+  MapStrandEffs f ( s ': strands ) = '(StrandName s, f (StrandEff s)) ': MapStrandEffs f strands
+
+effmapRopeRec :: (EffFunctor f)
+              => Rec (Weaver interp) strands
+              -> Rec (Weaver (f interp)) (MapStrandEffs f strands)
+effmapRopeRec RNil = RNil
+effmapRopeRec (Weaver w :& rest) = Weaver (effmap w) :& effmapRopeRec rest
+
+-- | When all the strands of a 'Rope' have the same type of ahead of time
+-- effects, we can run them. See 'splitRope' to isolate some strands in a 'Rope'
+unwrapRopeRunner :: (EffFunctor f)
+                 => RopeRunner Rec (MapStrandEffs f strands) (f core) core
+                :-> RopeRunner Rec strands core core
+unwrapRopeRunner (RopeRunner f) = RopeRunner $ f . effmapRopeRec
+{-# INLINE unwrapRopeRunner #-}
+
+unwrapSomeStrands :: (EffFunctor f, RMap (MapStrandEffs f mantle1))
+                  => (f core' :-> interp)
+                     -- ^ How to run the extracted EffFunctor layer
+                  -> (RopeRunner Rec mantle2 interp core :-> core')
+                     -- ^ What to do with the remaining strands (those not
+                     -- wrapped)
+                  -> RopeRunner Rec (MapStrandEffs f mantle1 ++ mantle2) interp core
+                     -- ^ The 'RopeRunner' to transform, where first strands all
+                     -- are wrapped in the @f@ EffFunctor
+                 :-> RopeRunner Rec mantle1                              core'  core'
+                     -- ^ The resulting 'RopeRunner', where 
+unwrapSomeStrands f g = unwrapRopeRunner . effdimap f g . splitRopeRunner
+{-# INLINE unwrapSomeStrands #-}
